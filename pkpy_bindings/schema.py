5c61fd6e7248e81c8305b523050863f8e242320b
blueloveTH <blueloveTH@foxmail.com>
2023-09-30 02:32:32 +0800

    Update schema.py

--- pkpy_bindings/schema.py
+++ pkpy_bindings/schema.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Union, Optional, Any, List, TypeVar, Callable, Type, cast
+from typing import Any, List, Union, Optional, TypeVar, Callable, Type, cast
 
 
 T = TypeVar("T")
@@ -10,14 +10,19 @@ def from_str(x: Any) -> str:
     return x
 
 
+def from_list(f: Callable[[Any], T], x: Any) -> List[T]:
+    assert isinstance(x, list)
+    return [f(y) for y in x]
+
+
+def to_class(c: Type[T], x: Any) -> dict:
+    assert isinstance(x, c)
+    return cast(Any, x).to_dict()
+
+
 def from_float(x: Any) -> float:
     assert isinstance(x, (float, int)) and not isinstance(x, bool)
     return float(x)
-
-
-def from_none(x: Any) -> Any:
-    assert x is None
-    return x
 
 
 def from_union(fs, x):
@@ -34,18 +39,13 @@ def to_float(x: Any) -> float:
     return x
 
 
-def from_list(f: Callable[[Any], T], x: Any) -> List[T]:
-    assert isinstance(x, list)
-    return [f(y) for y in x]
-
-
-def to_class(c: Type[T], x: Any) -> dict:
-    assert isinstance(x, c)
-    return cast(Any, x).to_dict()
-
-
 def from_int(x: Any) -> int:
     assert isinstance(x, int) and not isinstance(x, bool)
+    return x
+
+
+def from_none(x: Any) -> Any:
+    assert x is None
     return x
 
 
@@ -54,7 +54,6 @@ class Alias:
     type: str
     name: str
     description: str
-    value: Optional[Union[float, str]] = None
 
     @staticmethod
     def from_dict(obj: Any) -> 'Alias':
@@ -62,16 +61,13 @@ class Alias:
         type = from_str(obj.get("type"))
         name = from_str(obj.get("name"))
         description = from_str(obj.get("description"))
-        value = from_union([from_float, from_str, from_none], obj.get("value"))
-        return Alias(type, name, description, value)
+        return Alias(type, name, description)
 
     def to_dict(self) -> dict:
         result: dict = {}
         result["type"] = from_str(self.type)
         result["name"] = from_str(self.name)
         result["description"] = from_str(self.description)
-        if self.value is not None:
-            result["value"] = from_union([to_float, from_str, from_none], self.value)
         return result
 
 
@@ -99,7 +95,7 @@ class Callback:
     name: str
     description: str
     return_type: str
-    params: Optional[List[Param]] = None
+    params: List[Param]
 
     @staticmethod
     def from_dict(obj: Any) -> 'Callback':
@@ -107,7 +103,7 @@ class Callback:
         name = from_str(obj.get("name"))
         description = from_str(obj.get("description"))
         return_type = from_str(obj.get("returnType"))
-        params = from_union([lambda x: from_list(Param.from_dict, x), from_none], obj.get("params"))
+        params = from_list(Param.from_dict, obj.get("params"))
         return Callback(name, description, return_type, params)
 
     def to_dict(self) -> dict:
@@ -115,8 +111,32 @@ class Callback:
         result["name"] = from_str(self.name)
         result["description"] = from_str(self.description)
         result["returnType"] = from_str(self.return_type)
-        if self.params is not None:
-            result["params"] = from_union([lambda x: from_list(lambda x: to_class(Param, x), x), from_none], self.params)
+        result["params"] = from_list(lambda x: to_class(Param, x), self.params)
+        return result
+
+
+@dataclass
+class Define:
+    name: str
+    type: str
+    value: Union[float, str]
+    description: str
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Define':
+        assert isinstance(obj, dict)
+        name = from_str(obj.get("name"))
+        type = from_str(obj.get("type"))
+        value = from_union([from_float, from_str], obj.get("value"))
+        description = from_str(obj.get("description"))
+        return Define(name, type, value, description)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["name"] = from_str(self.name)
+        result["type"] = from_str(self.type)
+        result["value"] = from_union([to_float, from_str], self.value)
+        result["description"] = from_str(self.description)
         return result
 
 
@@ -165,6 +185,32 @@ class EnumElement:
 
 
 @dataclass
+class Function:
+    name: str
+    description: str
+    return_type: str
+    params: Optional[List[Param]] = None
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Function':
+        assert isinstance(obj, dict)
+        name = from_str(obj.get("name"))
+        description = from_str(obj.get("description"))
+        return_type = from_str(obj.get("returnType"))
+        params = from_union([lambda x: from_list(Param.from_dict, x), from_none], obj.get("params"))
+        return Function(name, description, return_type, params)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["name"] = from_str(self.name)
+        result["description"] = from_str(self.description)
+        result["returnType"] = from_str(self.return_type)
+        if self.params is not None:
+            result["params"] = from_union([lambda x: from_list(lambda x: to_class(Param, x), x), from_none], self.params)
+        return result
+
+
+@dataclass
 class Struct:
     name: str
     description: str
@@ -188,32 +234,32 @@ class Struct:
 
 @dataclass
 class Header:
-    defines: List[Alias]
+    defines: List[Define]
     structs: List[Struct]
     aliases: List[Alias]
     enums: List[EnumElement]
     callbacks: List[Callback]
-    functions: List[Callback]
+    functions: List[Function]
 
     @staticmethod
     def from_dict(obj: Any) -> 'Header':
         assert isinstance(obj, dict)
-        defines = from_list(Alias.from_dict, obj.get("defines"))
+        defines = from_list(Define.from_dict, obj.get("defines"))
         structs = from_list(Struct.from_dict, obj.get("structs"))
         aliases = from_list(Alias.from_dict, obj.get("aliases"))
         enums = from_list(EnumElement.from_dict, obj.get("enums"))
         callbacks = from_list(Callback.from_dict, obj.get("callbacks"))
-        functions = from_list(Callback.from_dict, obj.get("functions"))
+        functions = from_list(Function.from_dict, obj.get("functions"))
         return Header(defines, structs, aliases, enums, callbacks, functions)
 
     def to_dict(self) -> dict:
         result: dict = {}
-        result["defines"] = from_list(lambda x: to_class(Alias, x), self.defines)
+        result["defines"] = from_list(lambda x: to_class(Define, x), self.defines)
         result["structs"] = from_list(lambda x: to_class(Struct, x), self.structs)
         result["aliases"] = from_list(lambda x: to_class(Alias, x), self.aliases)
         result["enums"] = from_list(lambda x: to_class(EnumElement, x), self.enums)
         result["callbacks"] = from_list(lambda x: to_class(Callback, x), self.callbacks)
-        result["functions"] = from_list(lambda x: to_class(Callback, x), self.functions)
+        result["functions"] = from_list(lambda x: to_class(Function, x), self.functions)
         return result
 
 
