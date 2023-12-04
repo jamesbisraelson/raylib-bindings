26fd86914fc2ea9348b38248dd214a36155bd79e
blueloveTH <blueloveth@foxmail.com>
2023-12-04 11:51:38 +0800

    fix CI

--- output/main.cpp
+++ output/main.cpp
@@ -1,5 +1,4 @@
 #include "pocketpy.h"
-
 using namespace pkpy;
 
 namespace pkpy{
@@ -9,13 +8,15 @@ namespace pkpy{
 int main(){
     VM* vm = new VM();
     add_module_raylib(vm);
-    const char* filepath = "../main.py";
-    Bytes b = vm->_import_handler(filepath);
+
+    std::string filepath = "../main.py";
+    int out_size;
+    unsigned char* b = vm->_import_handler(filepath.c_str(), filepath.size(), &out_size);
     if(!b){
         std::cerr << "Error: " << filepath << " not found" << std::endl;
         return 1;
     }
-    vm->exec(b.str());
+    vm->exec(std::string_view((char*)b, out_size)); 
 
     delete vm;
     return 0;
