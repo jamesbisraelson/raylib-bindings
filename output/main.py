df88af829cb44fda982174bd0e2705a9579ce921
blueloveTH <blueloveTH@foxmail.com>
2024-01-05 23:17:36 +0800

    ...

--- output/main.py
+++ output/main.py
@@ -1,14 +1,14 @@
-import raylib
+import raylib as rl
 
-raylib.InitWindow(800, 450, "raylib [core] example - basic window")
+rl.InitWindow(800, 450, "raylib [core] example - basic window")
 
-RAYWHITE = raylib.Color(34, 34, 34, 255)
-LIGHTGRAY = raylib.Color(200, 200, 200, 255)
+RAYWHITE = rl.Color(34, 34, 34, 255)
+LIGHTGRAY = rl.Color(200, 200, 200, 255)
 
-while not raylib.WindowShouldClose():
-    raylib.BeginDrawing()
-    raylib.ClearBackground(RAYWHITE)
-    raylib.DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY)
-    raylib.EndDrawing()
+while not rl.WindowShouldClose():
+    rl.BeginDrawing()
+    rl.ClearBackground(RAYWHITE)
+    rl.DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY)
+    rl.EndDrawing()
 
-raylib.CloseWindow()
+rl.CloseWindow()
