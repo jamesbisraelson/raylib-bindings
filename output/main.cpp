f6c9d75eceedbb5d8f44b8548ab9861075ef9226
James Israelson <jamesbisraelson@gmail.com>
2024-06-25 13:49:50 -0300

    get main py file from args

--- output/main.cpp
+++ output/main.cpp
@@ -1,3 +1,4 @@
+#include <filesystem>
 #include "pocketpy.h"
 using namespace pkpy;
 
@@ -5,11 +6,17 @@ namespace pkpy{
     void add_module_raylib(VM* vm);
 }
 
-int main(){
+int main(int argc, char** argv) {
     VM* vm = new VM();
     add_module_raylib(vm);
 
-    std::string filepath = "main.py";
+    std::string filepath;
+    if (argc > 1) {
+        filepath = argv[1];
+    } else {
+        filepath = "main.py";
+    }
+
     int out_size;
     unsigned char* b = vm->_import_handler(filepath.c_str(), &out_size);
     if(!b){
